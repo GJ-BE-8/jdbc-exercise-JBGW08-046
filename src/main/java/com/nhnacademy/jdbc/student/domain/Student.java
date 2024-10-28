396f99ae6c5bd17c91cf2e43c80e960421656983
thr13 <skhrnt2945@gmail.com>
2024-10-28 13:19:27 +0900

    :art: CRUD 제작

--- src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
+++ src/main/java/com/nhnacademy/jdbc/student/domain/Student.java
@@ -15,4 +15,64 @@ public class Student {
     private final LocalDateTime createdAt;
 
     //todo#0 필요한 method가 있다면 추가합니다.
+
+    public Student(String id, String name, GENDER gender, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public Student(String id, String name, GENDER gender, Integer age, LocalDateTime createdAt) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.createdAt = createdAt;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public GENDER getGender() {
+        return gender;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id) && Objects.equals(name, student.name) && gender == student.gender && Objects.equals(age, student.age) && Objects.equals(createdAt, student.createdAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, gender, age, createdAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", createdAt=" + createdAt +
+                '}';
+    }
 }
